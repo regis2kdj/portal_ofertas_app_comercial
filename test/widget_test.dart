d53711dee5720e095cebe720ba365c61fb2dbd4a
regis2kdj <regis2kdj@gmail.com>
2020-09-18 15:29:19 -0500

    Pantallas de bienvenida, inicio de sesion y registro de usuario
    Adicion de google fonts
    
    Configuracion inicial de clase principal

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:portal_ofertas_app_comercial/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(OfertasAppComercial());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
